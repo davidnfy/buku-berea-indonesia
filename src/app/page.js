5f80f08a7976ab5eac59e804779ef9527a959d6c
welly-im <wellyimanuel86@gmail.com>
2025-06-02 15:49:35 +0700

    add swiper

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,60 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// page.js
+"use client";
 
-export default function Home() {
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "./book.css";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCoverflow, Pagination } from "swiper/modules";
+
+export default function BookPage() {
+  const swiperImages = [
+    "/buku/01.jpg",
+    "/buku/02.jpg",
+    "/buku/03.jpg",
+    "/buku/04.jpg",
+    "/buku/05.jpg",
+    "/buku/06.jpg",
+    "/buku/01.jpg",
+    "/buku/02.jpg",
+    "/buku/03.jpg",
+    "/buku/04.jpg",
+    "/buku/05.jpg",
+    "/buku/06.jpg",
+  ];
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="container py-5">
+      <div className="row">
+        <div className="col-lg-12 mb-5">
+          <Swiper
+            effect="coverflow"
+            grabCursor={true}
+            centeredSlides={true}
+            slidesPerView="auto"
+            loop={true}
+            loopedslides={swiperImages.length + 5}
+            coverflowEffect={{
+              rotate: 0,
+              stretch: -20,
+              depth: 100,
+              modifier: 2,
+              slideShadows: true,
+            }}
+            pagination={{ clickable: true }}
+            modules={[EffectCoverflow, Pagination]}
+            className="mySwiper"
           >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+            {swiperImages.map((src, index) => (
+              <SwiperSlide key={index} className="rounded">
+                <img src={src} alt={`slide-${index}`} className="rounded" />
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
